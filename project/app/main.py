9a43dd3a5f5d7f7fdb00a2eb87e99879c525760f
sanggusti <gustiowinata0@gmail.com>
2021-06-25 16:44:50 +0700

    first commit

---
+++ project/app/main.py
@@ -0,0 +1,31 @@
+import logging
+
+from fastapi import FastAPI
+
+from app.api import ping, summaries  # updated
+from app.db import init_db
+
+
+log = logging.getLogger("uvicorn")
+
+
+def create_application() -> FastAPI:
+    application = FastAPI()
+    application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])  # new
+
+    return application
+
+
+app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
