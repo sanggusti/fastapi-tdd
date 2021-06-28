7b38ee5be380ae955ba79bf839c1fe08ebc7f3b5
sanggusti <gustiowinata0@gmail.com>
2021-06-28 10:21:41 +0700

    Adding summarizer, tests, configure crud and summaries

---
+++ project/app/summarizer.py
@@ -0,0 +1,25 @@
+import asyncio
+
+import nltk
+from newspaper import Article
+
+from app.models.tortoise import TextSummary
+
+
+async def generate_summary(summary_id: int, url: str) -> None:
+    article = Article(url)
+    article.download()
+    article.parse()
+
+    try:
+        nltk.data.find("tokenizers/punkt")
+    except LookupError:
+        nltk.download("punkt")
+    finally:
+        article.nlp()
+
+    summary = article.summary
+
+    await asyncio.sleep(10)
+
+    await TextSummary.filter(id=summary_id).update(summary=summary)
--- project/tests/test_summaries_unit.py
+++ project/tests/test_summaries_unit.py
@@ -24,22 +24,18 @@ def test_create_summary(test_app, monkeypatch):
     assert response.json() == test_response_payload
 
 
-def test_create_summaries_invalid_json(test_app):
-    response = test_app.post("/summaries/", data=json.dumps({}))
-    assert response.status_code == 422
-    assert response.json() == {
-        "detail": [
-            {
-                "loc": ["body", "url"],
-                "msg": "field required",
-                "type": "value_error.missing",
-            }
-        ]
-    }
+def test_create_summary(test_app_with_db, monkeypatch):
+    def mock_generate_summary(summary_id, url):
+        return None
 
-    response = test_app.post("/summaries/", data=json.dumps({"url": "invalid://url"}))
-    assert response.status_code == 422
-    assert response.json()["detail"][0]["msg"] == "URL scheme not permitted"
+    monkeypatch.setattr(summaries, "generate_summary", mock_generate_summary)
+
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
+
+    assert response.status_code == 201
+    assert response.json()["url"] == "https://foo.bar"
 
 
 def test_read_summary(test_app, monkeypatch):
